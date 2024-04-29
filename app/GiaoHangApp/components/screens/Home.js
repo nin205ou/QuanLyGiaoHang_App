e8af172cf5db02fa28c6a54db00782a7e3848182
ThuHuyenK20 <115906040+ThuHuyenK20@users.noreply.github.com>
2024-04-29 16:24:01 +0700

    Home.js

--- app/GiaoHangApp/components/screens/Home.js
+++ app/GiaoHangApp/components/screens/Home.js
@@ -1,5 +1,6 @@
 import * as React from 'react';
-import { View,Text,TouchableOpacity } from 'react-native';
+import { View, Text, TextInput, TouchableOpacity } from 'react-native';
+import { AntDesign } from '@expo/vector-icons';
 
 
 export default function Home({navigation}){
@@ -9,7 +10,15 @@ export default function Home({navigation}){
   };
   return(
     <View style={{flex:1,alignItems:'center',justifyContent:'center'}}>
-         <Text onPress={() => alert("this home")} style={{ fontSize: 26, fontWeight: 'bold' }}>Home</Text>
+        <View style={{ position: 'absolute', top: 20, left: 20, width: '66%' , flexDirection: 'row', alignItems: 'center'}}>
+          <TextInput
+            style={{ flex: 1, height: 35, borderColor: 'gray', borderWidth: 1, marginRight: 10, paddingLeft: 10 }}
+            placeholder="Mẫ Vận đơn ..."
+          />
+          <TouchableOpacity onPress={() => alert("Chưa nhập nội dung !!!")} style={{ padding: 10,backgroundColor: 'green'  }}>
+            <AntDesign name="search" size={15} color="black" />
+          </TouchableOpacity>
+        </View>
          
          <TouchableOpacity style={{ position: 'absolute', top: 20, right: 20 }} onPress={handleLogin}>
             <Text style={{ fontSize: 16, color: 'blue' }}>Login</Text>
