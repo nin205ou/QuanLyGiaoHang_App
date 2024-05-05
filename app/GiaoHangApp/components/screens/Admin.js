77172bd6ad1a21622e6c831e4b383428345dc774
ThuHuyenK20 <115906040+ThuHuyenK20@users.noreply.github.com>
2024-05-06 01:15:51 +0700

    Admin.js

---
+++ app/GiaoHangApp/components/screens/Admin.js
@@ -0,0 +1,28 @@
+import * as React from 'react';
+import { View, Text, TextInput, TouchableOpacity } from 'react-native';
+import { AntDesign } from '@expo/vector-icons';
+
+
+export default function Amin({navigation}){
+
+  const handleLogin = () => {
+    navigation.navigate('Login');
+  };
+  return(
+    <View style={{flex:1,alignItems:'center',justifyContent:'center'}}>
+              <Text style={{ position: 'absolute', top: 70, left: 20, fontSize: 20, fontWeight: 'bold', color: 'black' }}>Admin</Text>
+        <View style={{ position: 'absolute', top: 110, left: 20, width: '66%' , flexDirection: 'row', alignItems: 'center'}}>
+          <TextInput
+            style={{ flex: 1, height: 35, borderColor: 'gray', borderWidth: 1, marginRight: 10, paddingLeft: 10 }}
+            placeholder="Mẫ Vận đơn ..."
+          />
+          <TouchableOpacity onPress={() => alert("Chưa nhập nội dung !!!")} style={{ padding: 10,backgroundColor: 'green'  }}>
+          <Text style={{ fontSize: 16, color: 'blue' }}>Search</Text>
+          </TouchableOpacity>
+        </View>
+            <TouchableOpacity style={{ position: 'absolute', top: 120, right: 20, marginLeft: 5 }} onPress={handleLogin}>
+            <Text style={{ fontSize: 16, color: 'blue' }}>Logout</Text>
+        </TouchableOpacity>
+    </View>
+  );
+}
